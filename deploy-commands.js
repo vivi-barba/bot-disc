3cdab5ec6db31ece25beb4f3d5dd0b35d896993c
Victória Barbosa <vitoriabbarbosa31@gmail.com>
2023-03-17 04:00:56 -0300

    feat: import const

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,3 +5,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 const {TOKEN, CLIENT_ID, GUILD_ID} = process.env;
 
+const fs = require("node:fs");
+const path = require("node:path");
+const commandsPath = path.join(__dirname, "slash_commands")
+const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
