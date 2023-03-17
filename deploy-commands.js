743c1eb382a2dd790dedd55503ed5caea62085f9
Victória Barbosa <vitoriabbarbosa31@gmail.com>
2023-03-17 03:59:11 -0300

    feat: import const

---
+++ deploy-commands.js
@@ -0,0 +1,7 @@
+const { REST, Routes} = require("discord.js")
+
+
+const dotenv = require('dotenv');
+dotenv.config();
+const {TOKEN, CLIENT_ID, GUILD_ID} = process.env;
+
--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const {TOKEN, CLIENT_ID, GUILD_ID} = process.env;
 //import slash command of file
 const fs = require("node:fs");
 const path = require("node:path");
-
 const commandsPath = path.join(__dirname, "slash_commands")
 const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"))
 
