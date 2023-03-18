3f6d893c509fb72a1cc660b8ef72b6fb84c86565
Victória Barbosa <vitoriabbarbosa31@gmail.com>
2023-03-18 13:49:34 -0300

    deploy slash commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -10,3 +10,32 @@ const path = require("node:path");
 const commandsPath = path.join(__dirname, "slash_commands")
 const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 
+const commands = [];
+
+for (const file in commandsFiles){
+    const command = require(`./slash_commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+// instância rest
+const rest = new REST({versin: "10"}).setToken(TOKEN);
+
+//fazendo o deploy
+
+(async () => {
+    try {
+        console.log(`Resetando ${commands.length} comandos...`);
+
+        //put
+        const data = await rest.put(
+            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+            {body: commands}
+        )
+        console.log("comandos registrados com sucesso!");
+        
+
+    }
+    catch (error){
+        console.error(error);
+    }
+})();
