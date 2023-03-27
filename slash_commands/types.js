b528c51ec2c12aed2c485d6a99f8530dc7a96e9c
Victória Barbosa <vitoriabbarbosa31@gmail.com>
2023-03-27 17:02:44 -0300

    deploy

--- slash_commands/types.js
+++ slash_commands/types.js
@@ -1,17 +1,51 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
 
 const exampleEmbed = new EmbedBuilder()
-	.setColor("Blue")
-	.setTitle("Tipos")
-	.addFields(
-        { name: '\u200B', value: '\u200B' },
-		{ name: 'String', value: 'Em qualquer linguagem de programação de computador, uma string é uma sequência de CARACTERES usada para representar texto.', inline: true },
-		{ name: 'Number', value: 'Number é um tipo de dado numérico no formato de ponto flutuante de 64 bits de precisão dupla (IEEE 754) . Em outras linguagens de programação existem diferentes tipos numéricos; por exemplo, Inteiros, Floats, Doubles ou Bignums.', inline: true },
-		{ name: 'Const', value: 'Declara uma constante de escopo de bloco, apenas de leitura.', inline: true },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Se ainda ficou com dúvidas', iconURL: 'https://i.imgur.com/AfFp7pu.png' , value: 'https://www.youtube.com/watch?v=sIXUNUqnoXI' },
-
-	)
+  .setColor("Blue")
+  .setTitle("Tipos")
+  .addFields(
+    { name: "\u200B", value: "\u200B" },
+    {
+      name: "String",
+      value:
+        "Em qualquer linguagem de programação de computador, uma string é uma sequência de CARACTERES usada para representar texto.",
+      inline: true,
+    },
+    {
+      name: "Number",
+      value:
+        "Number é um tipo de dado numérico no formato de ponto flutuante de 64 bits de precisão dupla (IEEE 754) . Em outras linguagens de programação existem diferentes tipos numéricos; por exemplo, Inteiros, Floats, Doubles ou Bignums.",
+      inline: true,
+    },
+    {
+      name: "Int",
+      value: "Declara uma constante de escopo de bloco, apenas de leitura.",
+      inline: true,
+    },
+    { name: "\u200B", value: "\u200B" },
+    {
+      name: "boolean",
+      value:
+        "Em qualquer linguagem de programação de computador, uma string é uma sequência de CARACTERES usada para representar texto.",
+      inline: true,
+    },
+    {
+      name: "Null",
+      value:
+        "Number é um tipo de dado numérico no formato de ponto flutuante de 64 bits de precisão dupla (IEEE 754) . Em outras linguagens de programação existem diferentes tipos numéricos; por exemplo, Inteiros, Floats, Doubles ou Bignums.",
+      inline: true,
+    },
+    {
+      name: "Undefined",
+      value: "Declara uma constante de escopo de bloco, apenas de leitura.",
+      inline: true,
+    },
+    {
+      name: "Se ainda ficou com dúvidas",
+      iconURL: "https://i.imgur.com/AfFp7pu.png",
+      value: "https://www.youtube.com/watch?v=7Isq6ekQT1k",
+    }
+  );
 	
 module.exports = {
     data: new SlashCommandBuilder()
